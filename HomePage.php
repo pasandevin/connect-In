5890e4d78dd5e9b263a7eb2fead4f32b2d3a1e47
Pasan Jayawardene <pj799571@gmail.com>
2021-08-31 20:28:40 +0530

    added links from main page to register and login pages

--- HomePage.php
+++ HomePage.php
@@ -6,5 +6,38 @@
 </head>
 
 <body>
+	<?php
+	if( isset ( $_REQUEST['Reg_Log'] ) ){
+	
+	$value = $_REQUEST['Reg_Log']; 
+	
+	if ($value == 'Reg'){
+		header('Location:register.php');
+		
+	}
+		elseif($value == 'Log'){
+			header('Location:login.php');
+			
+		}
+	
+}
+?>
+
+	
+	
+	
+	<h1>Welcome to connect-In</h1>
+<form  method="get" action="<?php $_SERVER['PHP_SELF'];?>">
+Get Started
+<button name="Reg_Log" type="submit" value="Reg">Register</button>
+	<button name="Reg_Log" type="submit" value="Log">Log-In</button>
+
+</form>
+
+
+
+</form>
+	
+
 </body>
 </html>
